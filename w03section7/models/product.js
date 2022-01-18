07b0909ff1379ac3b9bb7d5ab8568beaad7cd475
kayleew93 <67721730+kayleew93@users.noreply.github.com>
2022-01-17 20:44:01 -0700

    initial commit week 3

--- w03section7/models/product.js
+++ w03section7/models/product.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
     save() {
         const p = path.join(
